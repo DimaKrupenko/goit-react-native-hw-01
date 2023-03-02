b22f967ecddbd5586af8b3acda5ca43616af862e
DimaKrupenko <dimakrupenko77@gmail.com>
2023-03-02 18:41:57 +0200

    upd: make camera

--- myNewProject/screens/main/CreatePostsScreen.js
+++ myNewProject/screens/main/CreatePostsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CreatePostScreen = () => {
   return (
     <View style={styles.conteiner}>
-      <Text>CreatePostScreen</Text>
+      <Camera style={styles.camera}>
+        <TouchableOpacity onPress={() => {}} style={styles.snapConteiner}>
+          <Text style={styles.snap}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
@@ -12,6 +19,22 @@ const CreatePostScreen = () => {
 const styles = StyleSheet.create({
   conteiner: {
     flex: 1,
+  },
+  camera: {
+    height: 300,
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  snap: {
+    color: 'white',
+  },
+  snapConteiner: {
+    borderWidth: 1,
+    borderColor: 'red',
+    marginTop: 200,
+    width: 70,
+    height: 70,
+    borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
