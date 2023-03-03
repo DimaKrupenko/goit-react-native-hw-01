df4feab53749d8664e6cd7fe07ee508d64f325b2
DimaKrupenko <dimakrupenko77@gmail.com>
2023-03-03 18:06:05 +0200

    upd: make camera

--- myNewProject/screens/main/CreatePostsScreen.js
+++ myNewProject/screens/main/CreatePostsScreen.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as MediaLibrary from 'expo-media-library';
 
 const CreatePostScreen = () => {
+  const [cameraRef, setCameraRef] = useState(null);
+  const [photo, setPhoto] = useState('');
+
+  const takePhoto = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      await MediaLibrary.createAssetAsync(photo.uri);
+      setPhoto(photo.uri);
+    }
+  };
+
   return (
     <View style={styles.conteiner}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity onPress={() => {}} style={styles.snapConteiner}>
+      <Camera
+        style={styles.camera}
+        ref={ref => {
+          setCameraRef(ref);
+        }}
+      >
+        {photo && (
+          <View style={styles.takePhotoConteiner}>
+            <Image
+              source={{ uri: photo }}
+              style={{ height: 200, width: 200 }}
+            />
+          </View>
+        )}
+
+        <TouchableOpacity onPress={takePhoto} style={styles.snapConteiner}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
@@ -21,8 +47,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
-    height: 300,
-    marginTop: 50,
+    // height: 300,
+    // marginTop: 50,
+    flex: 1,
     alignItems: 'center',
   },
   snap: {
@@ -31,12 +58,19 @@ const styles = StyleSheet.create({
   snapConteiner: {
     borderWidth: 1,
     borderColor: 'red',
-    marginTop: 200,
+    marginTop: 650,
     width: 70,
     height: 70,
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  takePhotoConteiner: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+    borderColor: '#fff',
+    borderWidth: 1,
   },
 });
 
