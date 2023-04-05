5a9af86596519a95bf947654bbebc63596272599
DimaKrupenko <dimakrupenko77@gmail.com>
2023-04-05 18:24:00 +0300

    upd

--- myNewProject/redux/auth/authOperations.js
+++ myNewProject/redux/auth/authOperations.js
@@ -1,20 +1,38 @@
 // import firebase from 'firebase/app';
 import 'firebase/auth';
-import app from '../../firebase/config';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { app } from '../../firebase/config';
 
+const auth = getAuth();
 const authSignUpUser =
   ({ name, email, password }) =>
   async (dispatch, getState) => {
     console.log(name, email, password);
-
-    try {
-      const user = await app
-        .auth()
-        .createUserWithEmailAndPassword(email, password);
-    } catch (error) {
-      console.log(error.message);
-    }
+    await createUserWithEmailAndPassword(auth, name, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
   };
+// const authSignUpUser =
+//   ({ name, email, password }) =>
+//   async (dispatch, getState) => {
+//     console.log(name, email, password);
+
+//     try {
+//       const user = await app
+//         .auth()
+//         .createUserWithEmailAndPassword(email, password);
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
 
 const authSignInUser = () => async (dispatch, getState) => {};
 
